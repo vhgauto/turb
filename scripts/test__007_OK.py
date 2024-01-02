eb85a905d4c0df1fbe5563670c62d300b5d0e3ba
vhgauto <victor.gauto@outlook.com>
2024-01-02 13:58:23 -0300

    agrego s2-msi

--- scripts/test__007_OK.py
+++ scripts/test__007_OK.py
@@ -1,12 +1,80 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# solicitudes HTTP
+import requests
+
+# manejo de datos
+import pandas as pd
+
+# fechas
+from datetime import datetime, timedelta
+
+# acceso a las credenciales
 import os
+
+# leo las credenciales, almacenadas en el archivo .env
+from dotenv import load_dotenv
+
+# acceso al token
+import certifi
+
+# lectura de JSON, se usa al obtener el token
 import json
+
+
+# base URL of the product catalogue
+catalogue_odata_url = "https://catalogue.dataspace.copernicus.eu/odata/v1"
+
+# search parameters
+collection_name = "SENTINEL-2"
+product_type = "S2MSI1C"
+max_cloud_cover = 1
+aoi = "POLYGON((20.888443 52.169721,21.124649 52.169721,21.124649 52.271099,20.888443 52.271099,20.888443 52.169721))"
+search_period_start = "2023-06-01T00:00:00.000Z"
+search_period_end = "2023-06-10T00:00:00.000Z"
+
+search_query = f"{catalogue_odata_url}/Products?$filter=Collection/Name eq '{collection_name}' and Attributes/OData.CSC.StringAttribute/any(att:att/Name eq 'productType' and att/OData.CSC.StringAttribute/Value eq '{product_type}') and OData.CSC.Intersects(area=geography'SRID=4326;{aoi}') and ContentDate/Start gt {search_period_start} and ContentDate/Start lt {search_period_end}"
+
+print(f"""\n{search_query.replace(' ', "%20")}\n""")
+
+response = requests.get(search_query).json()
+result = pd.DataFrame.from_dict(response["value"])
+
+# print first 3 results
+result.head(3)
 
 username = os.environ['S2MSI_USERNAME']
 password = os.environ['S2MSI_PASSWORD']
 
+auth_server_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
+data = {
+    "client_id": "cdse-public",
+    "grant_type": "password",
+    "username": username,
+    "password": password,
+}
 
-print('username', username)
-print('password', password)
+response = requests.post(auth_server_url, data=data, verify=True, allow_redirects=False)
+access_token = json.loads(response.text)["access_token"]
+
+# Select identifier of the first product
+product_identifier = result.iloc[0, 1]
+product_name = result.iloc[0, 2]
+
+# Establish session
+session = requests.Session()
+session.headers["Authorization"] = f"Bearer {access_token}"
+
+# Nodes() method lets us traverse the directory tree and retrieve single file from the product
+url = f"{catalogue_odata_url}/Products({product_identifier})/Nodes({product_name})/Nodes(MTD_MSIL1C.xml)/$value"
+response = session.get(url, allow_redirects=False)
+while response.status_code in (301, 302, 303, 307):
+    url = response.headers["Location"]
+    response = session.get(url, allow_redirects=False)
+
+file = session.get(url, verify=False, allow_redirects=True)
+
+# Save the product in home directory
+outfile =  "MTD_MSIL1C.xml"
+outfile.write_bytes(file.content)
