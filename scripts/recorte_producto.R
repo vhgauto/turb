800ca7bef6e4f57dabbf26a04d2ea62f4e2c54b4
vhgauto <victor.gauto@outlook.com>
2024-01-04 13:02:29 -0300

    remuevo función para extraer .zip

--- scripts/recorte_producto.R
+++ scripts/recorte_producto.R
@@ -6,8 +6,8 @@ source("scripts/funciones.R")
 # cargo los paquetes
 paquetes()
 
-# extraigo el producto .zip
-extraigo_zip()
-
 # recorto el producto a roi y guardo stack
 recorte()
+
+# genero imagen RGB
+imagen_rgb()
